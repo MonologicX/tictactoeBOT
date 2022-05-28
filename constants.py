9091543ea8554648c6e63b99be097d1c9511049c
MonologicX <monologicx@gmail.com>
2022-05-28 14:39:24 -0500

    Full Refactoring of the TICTACTOE code into a class

--- constants.py
+++ constants.py
@@ -6,6 +6,7 @@ WINWIDTH, WINHEIGHT = 720, 720
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 TOPLEFT = pygame.Rect(50, 50, 200, 200)
--- tictactoe.py
+++ tictactoe.py
@@ -3,189 +3,161 @@ import sys
 import time
 import random
 from constants import *
-global turn, gameOver, winner
-pygame.init()
 
-WIN = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
-pygame.display.set_caption("TicTacToe")
-font = pygame.font.SysFont('Sans', 25)
-pygame.font.init()
+class TicTacToe:
+    def __init__(self, starter=1):
+        pygame.init()
+        pygame.display.set_caption("TicTacToe")
 
-turn = 1
-gameOver = False
-winner = 0
+        self.WIN = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+        self.FONT = pygame.font.SysFont('Sans', 25)
+        pygame.font.init()
 
-board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
 
-boardRects = [
-    [TOPLEFT, TOPMID, TOPRIGHT],
-    [MIDLEFT, MIDMID, MIDRIGHT],
-    [BOTLEFT, BOTMID, BOTRIGHT]
-]
+        self.boardRects = [
+            [TOPLEFT, TOPMID, TOPRIGHT],
+            [MIDLEFT, MIDMID, MIDRIGHT],
+            [BOTLEFT, BOTMID, BOTRIGHT]
+        ]
+        self.turn = starter
+        self.gameOver = False
+        self.winner = 0
+        self.clock = pygame.time.Clock()
 
-def drawGrid():
-    pygame.draw.rect(WIN, BLACK, pygame.Rect(250, 50, 10, 620))
-    pygame.draw.rect(WIN, BLACK, pygame.Rect(460, 50, 10, 620))
-    pygame.draw.rect(WIN, BLACK, pygame.Rect(50, 250, 620, 10))
-    pygame.draw.rect(WIN, BLACK, pygame.Rect(50, 460, 620, 10))
+    def draw(self, gameOver=False):
 
-def drawSquares():
+        if gameOver:
+            if self.winner == 1:
+                winMessage = self.FONT.render("You Won", True, GREEN)
+            elif self.winner == -1:
+                winMessage = self.FONT.render("Bot Won", True, GREEN)
+            elif self.winner == 0:
+                winMessage = self.FONT.render("DRAW", True, GREEN)
 
-    for row in range(len(board)):
-        for square in range(len(board[row])):
-            if board[row][square] == 1:
-                pygame.draw.rect(WIN, RED, boardRects[row][square])
-            elif board[row][square] == -1:
-                pygame.draw.rect(WIN, BLUE, boardRects[row][square])
-
-def draw():
-
-    WIN.fill(WHITE)
-    drawGrid()
-    drawSquares()
-    pygame.display.update()
-
-def clickBox(row, column, currentTurn):
-
-    if board[row][column] == 0:
-
-        #Place Square
-        board[row][column] = currentTurn
-
-        #Change Turns
-        if currentTurn == 1:
-            currentTurn = -1
-        elif currentTurn == -1:
-            currentTurn = 1
-
-    return currentTurn
-
-def main():
-
-    clock = pygame.time.Clock()
-    clock.tick(FPS)
-
-    gameOver = False
-
-    turn = 1
-
-    while gameOver == False:
-
-        if turn == -1:
-            turn = botTurn(turn)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameOver = True
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouseCoords = pygame.mouse.get_pos()
-                xClick = 0
-                yClick = 0
-                print("MouseX: {0}, MouseY: {1}".format(mouseCoords[0], mouseCoords[1]))
-
-                if mouseCoords[0] < 250:
-                    xClick = 0
-                elif mouseCoords[0] > 460:
-                    xClick = 2
-                elif 250 < mouseCoords[0] < 460:
-                    xClick = 1
-
-                if mouseCoords[1] < 250:
-                    yClick = 0
-                elif mouseCoords[1] > 460:
-                    yClick = 2
-                elif 250 < mouseCoords[1] < 460:
-                    yClick = 1
-
-                turn = clickBox(yClick, xClick, turn)
-
-        draw()
-    while gameOver == True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    main()
-        WIN.fill(WHITE)
-
-        if winner == 1:
-            winMessage = font.render("You Won", True, BLACK)
-        elif winner == -1:
-            winMessage = font.render("The Bot Won", True, BLACK)
-        elif winner == 0:
-            winMessage = font.render("DRAW", True, BLACK)
-
-        winMessage.center(WINWIDTH / 2, WINHEIGHT / 2)
-        WIN.blit(winMessage, winMessage.get_rect())
-
+            self.WIN.blit(winMessage, winMessage.get_rect(center=(WINWIDTH / 2, WINHEIGHT / 2)))
+        else:
+            self.WIN.fill(WHITE)
+            self.drawGrid()
+            self.drawSquares()
 
         pygame.display.update()
 
-def checkForWin(turn):
-    for i in range(3):
-        rowSum = board[i][0] + board[i][1] + board[i][2]
-        if abs(rowSum) == 3:
-            winner = turn
-            gameOver = True
-        columnSum = board[0][i] + board[1][i] + board[2][i]
-        if abs(columnSum) == 3:
-            winner = turn
-            gameOver = True
+    def drawGrid(self):
+        pygame.draw.rect(self.WIN, BLACK, pygame.Rect(250, 50, 10, 620))
+        pygame.draw.rect(self.WIN, BLACK, pygame.Rect(460, 50, 10, 620))
+        pygame.draw.rect(self.WIN, BLACK, pygame.Rect(50, 250, 620, 10))
+        pygame.draw.rect(self.WIN, BLACK, pygame.Rect(50, 460, 620, 10))
 
+    def drawSquares(self):
+        for row in range(len(self.board)):
+            for square in range(len(self.board[row])):
+                if self.board[row][square] == 1:
+                    pygame.draw.rect(self.WIN, RED, self.boardRects[row][square])
+                elif self.board[row][square] == -1:
+                    pygame.draw.rect(self.WIN, BLUE, self.boardRects[row][square])
 
-def botTurn(turn):
+    def clickBox(self, box):
 
-    botRow = 0
-    botColumn = 0
+        boxIndexes = self.index2D(self.boardRects, box)
+        if self.board[boxIndexes[0]][boxIndexes[1]] == 0:
 
-    for i in range(3):
-        rowSum = board[i][0] + board[i][1] + board[i][2]
-        if abs(rowSum) == 2:
-            for j in range(3):
-                if board[i][j] == 0:
-                    botRow = i
-                    botColumn = j
-                    return clickBox(botRow, botColumn, turn)
+            self.board[boxIndexes[0]][boxIndexes[1]] = self.turn
+
+            if self.checkForWin():
+                self.winner = self.turn
+                self.gameOver = True
+            elif self.checkForDraw():
+                self.winner = 0
+                self.gameOver = True
+
+            if self.turn == 1:
+                self.turn = -1
+            elif self.turn == -1:
+                self.turn = 1
+
+    def index2D(self, array, value):
+        for i in range(len(array)):
+            if value in array[i]:
+                return i, array[i].index(value)
+
+    def resetBoard(self):
+
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+
+    def botTurn(self):
+        self.clickBox(self.boardRects[random.randint(0, 2)][random.randint(0, 2)])
+
+    def checkForWin(self):
+        win = False
+
+        for i in range(3):
+            if abs(sum(self.board[i])) == 3:
+                win = True
+            if abs(self.board[0][i] + self.board[1][i] + self.board[2][i]) == 3:
+                win = True
+
+        return win
+    def checkForDraw(self):
+
+        spaceAvailiable = False
+        for row in self.board:
+            for square in row:
+                if square == 0:
+                    spaceAvailiable = True
+
+        if spaceAvailiable:
+            return False
         else:
-            print("Rand R")
-            botRow = random.randint(0, 2)
-            botColumn = random.randint(0, 2)
+            return True
 
-    for i in range(3):
-        columnSum = board[0][i] + board[1][i] + board[2][i]
-        if abs(columnSum) == 3:
-            winner = turn
-            gameOver = True
-        if abs(columnSum) == 2:
-            for j in range(3):
-                print(j)
-                if board[j][i] == 0:
-                    botRow = j
-                    botColumn = i
-                    return clickBox(botRow, botColumn, turn)
-        else:
-            print("Rand C")
-            botRow = random.randint(0, 2)
-            botColumn = random.randint(0, 2)
+    def start(self):
 
-    gameFinished = True
-    for row in board:
-        for square in row:
-            if square == 0:
-                gameFinished = False
-    gameOver = gameFinished
+        self.clock.tick(FPS)
 
+        self.gameOver = False
+        self.turn = 1
+        self.winner = 0
+        self.resetBoard()
 
-    print("Row: {0}, Column: {1}".format(botRow, botColumn))
-    newTurn = clickBox(botRow, botColumn, turn)
-    checkForWin(turn)
-    return newTurn
+        while self.gameOver == False:
 
-main()
+            if self.turn == -1:
+                self.botTurn()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gameOver = True
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouseCoords = pygame.mouse.get_pos()
+
+                    for row in self.boardRects:
+                        for square in row:
+                            if square.collidepoint(mouseCoords) == True:
+                                self.clickBox(square)
+
+            self.draw()
+
+        while self.gameOver == True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gameOver = True
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        self.start()
+            self.draw(gameOver=True)
+
+ttt = TicTacToe()
+ttt.start()
